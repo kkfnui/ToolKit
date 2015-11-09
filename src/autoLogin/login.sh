0e8dba250602143a6d165d539a9c707287fa7e14
吕飞 <kkfnui@gmail.com>
2015-11-09 13:36:10 +0800

    Ensure local config file won't upload to github
    Local config file should contains `.local.`

--- src/autoLogin/login.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-python /Users/lvfei/Code/ToolKit/src/autoLogin/autoLogin.py -c /Users/lvfei/Code/ToolKit/conf/local.json -s $1
--- src/speedyScp/speedyScp.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-
-python /Users/lvfei/Code/ServerToolKit/src/speedyScp.py -c /Users/lvfei/Code/ServerToolKit/src/local.json -f $1 -l test41,test104,test105,test125 -d $2
